cf28203e99a52074261fc9bc7ffc1b675ff6bfff
jiangfan wu <326737089@qq.com>
2023-09-20 11:35:16 +0800

    change Threadpool_test
    Signed-off-by: jiangfan wu <326737089@qq.com>

--- Threadpool_test.cpp
+++ Threadpool_test.cpp
@@ -29,10 +29,10 @@ int main() {
     pool.submitTask(std::make_shared<MyTask>());
     pool.submitTask(std::make_shared<MyTask>());
     pool.submitTask(std::make_shared<MyTask>());
-    pool.submitTask(std::make_shared<MyTask>());
     // 当有 10 个任务的时候，会有 8个任务成功，2个任务会失败
 
     // 有四个线程
     getchar();
 //    std::this_thread::sleep_for(std::chrono::seconds(5));
+    return 0;
 }
